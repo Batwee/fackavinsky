365bd5f8d8a0d1a7ed3b4cbc61769ec7ffb3f65e
Baptiste GARAY <baptiste.garay@gmail.com>
2018-11-27 14:53:06 +0100

    Add files via upload
    Iframe youtube/spotify/soundcloud ... + Essaie d'une correction d'un questionnaire HTML en PHP

---
+++ Kavinsky/correction.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+  <title>Quizz Kavinsky</title>
+ </head>
+ <body>
+ Les bonnes réponses étaient :
+<p>2013</p>
+<p>1</p>
+<p>OutRun</p>
+<p>Daftpunk</p>
+<p>41</p>
+ 
+ 
+ <script type="text/javascript">
+function calculer () {      score = q1 + q2 +q3 +q4+q5;  
+    alert("Score : " + score)      }
+</script>
+
+ </body>
+</html>
---
+++ Kavinsky/quizz.php
@@ -0,0 +1,148 @@
+
+
+<div>
+
+		<h1>Kavinsky</h1>
+		
+		<form action="correction.php" method="post" id="quiz">
+		
+            <ol>
+            
+                <li>
+                
+                    <h3>Quand est sorti le premier album de Kavinsky?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-1" id="question-1A" value="A" />
+                        <label for="question-1A">A) 2003</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1" id="question-1B" value="B" />
+                        <label for="question-1B">B) 2016</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1" id="question-1C" value="C" />
+                        <label for="question-1C">C) 2013</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1" id="question-1D" value="D" />
+                        <label for="question-1D">D) 2002</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Combien d'album a t'il sorti?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-2" id="question-2A" value="A" />
+                        <label for="question-2A">A) 1</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2" id="question-2B" value="B" />
+                        <label for="question-2B">B) 2</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2" id="question-2C" value="C" />
+                        <label for="question-2C">C) 5</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2" id="question-2D" value="D" />
+                        <label for="question-2D">D) 7</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Quel est le nom du premier album de Kavinsky?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-3" id="question-3A" value="A" />
+                        <label for="question-3A">A) OutRun</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3" id="question-3B" value="B" />
+                        <label for="question-3B">B) Lastday</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3" id="question-3C" value="C" />
+                        <label for="question-3C">C) Nightcall</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3" id="question-3D" value="D" />
+                        <label for="question-3D">D) Oddlook</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Avec quel artiste a t'il fait un feat?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-4" id="question-4A" value="A" />
+                        <label for="question-4A">A)TheWeeknd</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4B" value="B" />
+                        <label for="question-4B">B) Mickael Jackson</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4C" value="C" />
+                        <label for="question-4C">C) 50cens</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4" id="question-4D" value="D" />
+                        <label for="question-4D">D) Rihanna</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Quel age a t'il?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-5" id="question-5A" value="A" />
+                        <label for="question-5A">A) 41</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5" id="question-5B" value="B" />
+                        <label for="question-5B">B) 34</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5" id="question-5C" value="C" />
+                        <label for="question-5C">C) 23</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5" id="question-5D" value="D" />
+                        <label for="question-5D">D) 54</label>
+                    </div>
+                
+                </li>
+            
+            </ol>
+            
+            <input type="submit" value="Réponse" />
+		
+		</form>
+	
+	</div>
+	
